e595619b9208c19f1f665e9e3948a3f8385fd4a4
Bob Saska <git@r35.net>
2013-04-16 09:34:18 -0500

    Making Concentration preset brighter.

--- Preset_Concentrate.py
+++ Preset_Concentrate.py
@@ -4,15 +4,15 @@ import PyHueAPI
 
 if __name__ == '__main__':
     preset = {
-        1: {'on': True, 'bri': 219, 'ct': 233},
-        3: {'on': True, 'bri': 219, 'ct': 233},
-        2: {'on': True, 'bri': 219, 'ct': 233},
-        5: {'on': True, 'bri': 219, 'ct': 233},
-        4: {'on': True, 'bri': 219, 'ct': 233},
-        7: {'on': True, 'bri': 219, 'ct': 233},
-        6: {'on': True, 'bri': 219, 'ct': 233},
-        9: {'on': True, 'bri': 219, 'ct': 233},
-        8: {'on': True, 'bri': 219, 'ct': 233},
+        1: {'on': True, 'bri': 255, 'ct': 233},
+        3: {'on': True, 'bri': 255, 'ct': 233},
+        2: {'on': True, 'bri': 255, 'ct': 233},
+        5: {'on': True, 'bri': 255, 'ct': 233},
+        4: {'on': True, 'bri': 255, 'ct': 233},
+        7: {'on': True, 'bri': 255, 'ct': 233},
+        6: {'on': True, 'bri': 255, 'ct': 233},
+        9: {'on': True, 'bri': 255, 'ct': 233},
+        8: {'on': True, 'bri': 255, 'ct': 233},
     }
     
     lights = PyHueAPI.Lights()
