5d7a81b40e9979f3f6efab0a11b43cf1b2a674e2
Bob Saska <git@r35.net>
2013-04-21 01:03:12 -0500

    Adding a proof of concept of manual fading between presets.  Need to generalize it to work with any colorspace.  Granted you probably won't be able to fade from one colorspace to another.

---
+++ Preset_Sleep1_Fading.py
@@ -0,0 +1,118 @@
+#!/usr/bin/python
+
+import PyHueAPI
+from time import sleep
+
+if __name__ == '__main__':
+    startPreset = {
+        1: {'on': True, 'xy': [0.6271, 0.3297], 'bri': 239},
+        3: {'on': True, 'xy': [0.6271, 0.3297], 'bri': 239},
+        2: {'on': True, 'xy': [0.6271, 0.3297], 'bri': 239},
+        5: {'on': True, 'xy': [0.3196, 0.1852], 'bri': 145},
+        4: {'on': True, 'xy': [0.2643, 0.1646], 'bri': 147},
+        7: {'on': True, 'xy': [0.2432, 0.1903], 'bri': 157},
+        6: {'on': True, 'xy': [0.2432, 0.1903], 'bri': 157},
+        9: {'on': True, 'xy': [0.3196, 0.1852], 'bri': 145},
+        8: {'on': True, 'xy': [0.2643, 0.1646], 'bri': 147},      
+    }
+    
+    endPreset = {
+        1: {'on': True, 'xy': [0.6271, 0.3297], 'bri': 239},
+        3: {'on': True, 'xy': [0.6271, 0.3297], 'bri': 239},
+        2: {'on': True, 'xy': [0.6271, 0.3297], 'bri': 239},
+        5: {'on': True, 'xy': [0.6271, 0.3297], 'bri': 239},
+        4: {'on': True, 'xy': [0.6271, 0.3297], 'bri': 239},
+        7: {'on': True, 'xy': [0.6271, 0.3297], 'bri': 238},
+        6: {'on': True, 'xy': [0.6271, 0.3297], 'bri': 239},
+        9: {'on': True, 'xy': [0.6271, 0.3297], 'bri': 238},
+        8: {'on': True, 'xy': [0.6271, 0.3297], 'bri': 238},
+    }
+    
+    endPreset = {
+        1: {'on': True, 'xy': [0.2000, 0.1000], 'bri': 239},
+        3: {'on': True, 'xy': [0.2000, 0.1000], 'bri': 239},
+        2: {'on': True, 'xy': [0.2000, 0.1000], 'bri': 239},
+        5: {'on': True, 'xy': [0.2000, 0.1000], 'bri': 239},
+        4: {'on': True, 'xy': [0.2000, 0.1000], 'bri': 239},
+        7: {'on': True, 'xy': [0.2000, 0.1000], 'bri': 238},
+        6: {'on': True, 'xy': [0.2000, 0.1000], 'bri': 239},
+        9: {'on': True, 'xy': [0.2000, 0.1000], 'bri': 238},
+        8: {'on': True, 'xy': [0.2000, 0.1000], 'bri': 238},
+    }
+    
+    deltas = {
+        1: {'x': 0, 'y': 0, 'xi': 0, 'yi': 0, 'b': 0, 'bi': 0},
+        2: {'x': 0, 'y': 0, 'xi': 0, 'yi': 0, 'b': 0, 'bi': 0},
+        3: {'x': 0, 'y': 0, 'xi': 0, 'yi': 0, 'b': 0, 'bi': 0},
+        4: {'x': 0, 'y': 0, 'xi': 0, 'yi': 0, 'b': 0, 'bi': 0},
+        5: {'x': 0, 'y': 0, 'xi': 0, 'yi': 0, 'b': 0, 'bi': 0},
+        6: {'x': 0, 'y': 0, 'xi': 0, 'yi': 0, 'b': 0, 'bi': 0},
+        7: {'x': 0, 'y': 0, 'xi': 0, 'yi': 0, 'b': 0, 'bi': 0},
+        8: {'x': 0, 'y': 0, 'xi': 0, 'yi': 0, 'b': 0, 'bi': 0},
+        9: {'x': 0, 'y': 0, 'xi': 0, 'yi': 0, 'b': 0, 'bi': 0}
+    }
+    
+    currentValues = {
+        1: [0, 0, 0],
+        2: [0, 0, 0],
+        3: [0, 0, 0],
+        4: [0, 0, 0],
+        5: [0, 0, 0],
+        6: [0, 0, 0],
+        7: [0, 0, 0],
+        8: [0, 0, 0],
+        9: [0, 0, 0]
+    }
+    
+    # In MINUTES
+    fadeLength = 10
+    timesPerMinute = 2
+    
+    ### DRAGONS
+    totalCycles = fadeLength * timesPerMinute
+    napBetweenCycles = 60 / timesPerMinute
+    
+    lights = PyHueAPI.Lights()
+    
+    print "Computing Deltas..."
+    for i in range(1,10):
+        """Compute all the deltas"""
+        # Deltas
+        deltas[i]['x'] = endPreset[i]['xy'][0] - startPreset[i]['xy'][0]
+        deltas[i]['y'] = endPreset[i]['xy'][1] - startPreset[i]['xy'][1]
+        deltas[i]['b'] = endPreset[i]['bri'] - startPreset[i]['bri']
+        
+        # Deltas per interval
+        deltas[i]['xi'] = (deltas[i]['x'] / fadeLength) / timesPerMinute
+        deltas[i]['yi'] = (deltas[i]['y'] / fadeLength) / timesPerMinute
+        deltas[i]['bi'] = (deltas[i]['b'] / fadeLength) / timesPerMinute
+        currentValues[i] = [
+            startPreset[i]['xy'][0],
+            startPreset[i]['xy'][1],
+            startPreset[i]['bri']
+            ]
+        tmp = lights.get(i)
+        tmp.bulkSetState({'xy': [currentValues[i][0], currentValues[i][1]], 'bri': currentValues[i][2]})
+                
+    print deltas
+    print ""
+    print ""
+        
+    print "Fading..."
+    for i in range(1, totalCycles):
+        sleep(napBetweenCycles)
+        print ""
+        print "Cycle %d of %d" % (i, totalCycles)
+        
+        for j in range(1,10):
+            newX = currentValues[j][0] + deltas[j]['xi']
+            newY = currentValues[j][1] + deltas[j]['yi']
+            newB = currentValues[j][2] + deltas[j]['bi']
+            
+            print "\tLight %d (x=%0.4f, y=%0.4f, b=%0.4f)" % (j, newX, newY, newB)
+            currentValues[j][0] = newX
+            currentValues[j][1] = newY
+            currentValues[j][2] = newB
+            
+            tmp = lights.get(j)
+            tmp.bulkSetState({'xy': [currentValues[j][0], currentValues[j][1]], 'bri': currentValues[j][2]})
