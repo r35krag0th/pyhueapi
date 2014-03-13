1ec8c781a58242a75049feca46d658d9f993c304
Bob Saska <git@r35.net>
2014-03-12 19:45:59 -0500

    Fixing wakeup and what not

---
+++ presets/all/Preset_FancyWakeup.py
@@ -0,0 +1,137 @@
+#!/usr/bin/python
+import os, sys
+
+# If you're running from the app root this will make it work
+sys.path.append(os.path.abspath(os.path.join(os.path.curdir, '..', '..')))
+
+# If you're running this from anywhere else this will make it work
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(sys.argv[0]), '..', '..')))
+
+import pyhueapi
+from pyhueapi.preset import Preset
+from time import sleep
+
+if __name__ == '__main__':
+    bottomFadeTime = 30
+    middleFadeTime = 35
+    topFadeTime = 40
+
+    bottomFadeTime *= 10
+    middleFadeTime *= 10
+    topFadeTime *= 10
+
+    preset0 = []
+    for i in range(1,18):
+        print i
+        preset0.append({'id': i, 'on': True, 'bri': 10, 'xy': [0.674, 0.322]})
+
+    preset1 = [
+        {'id': 1, 'on': True, 'bri': 10, 'xy': [0.674, 0.322], 'transitiontime': topFadeTime},
+        {'id': 3, 'on': True, 'bri': 10, 'xy': [0.674, 0.322], 'transitiontime': topFadeTime},
+
+        {'id': 4, 'on': True, 'bri': 10, 'xy': [0.674, 0.322], 'transitiontime': bottomFadeTime},
+        {'id': 5, 'on': True, 'bri': 10, 'xy': [0.674, 0.322], 'transitiontime': middleFadeTime},
+        {'id': 6, 'on': True, 'bri': 10, 'xy': [0.674, 0.322], 'transitiontime': topFadeTime},
+
+        {'id': 8, 'on': True, 'bri': 10, 'xy': [0.674, 0.322], 'transitiontime': bottomFadeTime},
+        {'id': 9, 'on': True, 'bri': 10, 'xy': [0.674, 0.322], 'transitiontime': middleFadeTime},
+        {'id': 7, 'on': True, 'bri': 10, 'xy': [0.674, 0.322], 'transitiontime': topFadeTime},
+
+        {'id': 2,  'on': True, 'bri': 10, 'xy': [0.674, 0.322], 'transitiontime': topFadeTime},
+        {'id': 10, 'on': True, 'bri': 10, 'xy': [0.674, 0.322], 'transitiontime': topFadeTime},
+        {'id': 12, 'on': True, 'bri': 10, 'xy': [0.674, 0.322], 'transitiontime': topFadeTime},
+
+        {'id': 11, 'on': True, 'bri': 10, 'xy': [0.674, 0.322], 'transitiontime': topFadeTime},
+        {'id': 13, 'on': True, 'bri': 10, 'xy': [0.674, 0.322], 'transitiontime': topFadeTime},
+        {'id': 14, 'on': True, 'bri': 10, 'xy': [0.674, 0.322], 'transitiontime': topFadeTime},
+
+        {'id': 15, 'on': True, 'bri': 10, 'xy': [0.674, 0.322], 'transitiontime': topFadeTime},
+        {'id': 16, 'on': True, 'bri': 10, 'xy': [0.674, 0.322], 'transitiontime': topFadeTime},
+        {'id': 17, 'on': True, 'bri': 10, 'xy': [0.674, 0.322], 'transitiontime': topFadeTime},
+    ]
+
+    preset2 = [
+        {'id': 1, 'on': True, 'bri': 100, 'xy': [0.5576, 0.4074], 'transitiontime': topFadeTime},
+        {'id': 2, 'on': True, 'bri': 100, 'xy': [0.5576, 0.4074], 'transitiontime': topFadeTime},
+        {'id': 3, 'on': True, 'bri': 100, 'xy': [0.5576, 0.4074], 'transitiontime': topFadeTime},
+
+        {'id': 4, 'on': True, 'bri': 100, 'xy': [0.5576, 0.4074], 'transitiontime': bottomFadeTime},
+        {'id': 5, 'on': True, 'bri': 100, 'xy': [0.5576, 0.4074], 'transitiontime': middleFadeTime},
+        {'id': 6, 'on': True, 'bri': 100, 'xy': [0.5576, 0.4074], 'transitiontime': topFadeTime},
+
+        {'id': 7, 'on': True, 'bri': 100, 'xy': [0.5576, 0.4074], 'transitiontime': topFadeTime},
+        {'id': 8, 'on': True, 'bri': 100, 'xy': [0.5576, 0.4074], 'transitiontime': bottomFadeTime},
+        {'id': 9, 'on': True, 'bri': 100, 'xy': [0.5576, 0.4074], 'transitiontime': middleFadeTime},
+
+        {'id': 10, 'on': True, 'bri': 10, 'xy': [0.5576, 0.4074], 'transitiontime': topFadeTime},
+
+        {'id': 11, 'on': True, 'bri': 50, 'xy': [0.5576, 0.4074], 'transitiontime': topFadeTime},
+        {'id': 12, 'on': True, 'bri': 50, 'xy': [0.5576, 0.4074], 'transitiontime': topFadeTime},
+        {'id': 13, 'on': True, 'bri': 50, 'xy': [0.5576, 0.4074], 'transitiontime': topFadeTime},
+        {'id': 14, 'on': True, 'bri': 50, 'xy': [0.5576, 0.4074], 'transitiontime': topFadeTime},
+
+        {'id': 15, 'on': True, 'bri': 50, 'xy': [0.5576, 0.4074], 'transitiontime': topFadeTime},
+        {'id': 16, 'on': True, 'bri': 50, 'xy': [0.5576, 0.4074], 'transitiontime': topFadeTime},
+        {'id': 17, 'on': True, 'bri': 100, 'xy': [0.5576, 0.4074], 'transitiontime': topFadeTime},
+    ]
+
+
+    preset3 = [
+        # Master N/S
+        {'id': 1,  'on': True, 'bri': 100, 'ct': 156, 'transitiontime': topFadeTime},
+        {'id': 3,  'on': True, 'bri': 100, 'ct': 156, 'transitiontime': topFadeTime},
+
+        # Master Lamp 1
+        {'id': 4,  'on': True, 'bri': 100, 'ct': 156, 'transitiontime': bottomFadeTime},
+        {'id': 5,  'on': True, 'bri': 100, 'ct': 156, 'transitiontime': middleFadeTime},
+        {'id': 6,  'on': True, 'bri': 100, 'ct': 156, 'transitiontime': topFadeTime},
+
+        # Master Lamp 2
+        {'id': 8,  'on': True, 'bri': 100, 'ct': 156, 'transitiontime': bottomFadeTime},
+        {'id': 9,  'on': True, 'bri': 100, 'ct': 156, 'transitiontime': middleFadeTime},
+        {'id': 7,  'on': True, 'bri': 100, 'ct': 156, 'transitiontime': topFadeTime},
+
+        # Living Room Lamp
+        {'id': 2,  'on': True, 'bri': 100, 'ct': 340, 'transitiontime': topFadeTime},
+        {'id': 10, 'on': True, 'bri': 100, 'ct': 340, 'transitiontime': topFadeTime},
+        {'id': 12, 'on': True, 'bri': 100, 'ct': 340, 'transitiontime': topFadeTime},
+
+        # Living Room Single Lamps
+        {'id': 11, 'on': True, 'bri': 100, 'ct': 340, 'transitiontime': topFadeTime},
+        {'id': 13, 'on': True, 'bri': 100, 'ct': 340, 'transitiontime': topFadeTime},
+        {'id': 14, 'on': True, 'bri': 100, 'ct': 340, 'transitiontime': topFadeTime},
+
+        # Dining Room Lights
+        {'id': 15, 'on': True, 'bri': 100, 'ct': 340, 'transitiontime': topFadeTime},
+        {'id': 16, 'on': True, 'bri': 100, 'ct': 340, 'transitiontime': topFadeTime},
+
+        # Coffee Table
+        {'id': 16, 'on': True, 'bri': 100, 'ct': 340, 'transitiontime': topFadeTime},
+    ]
+
+    preset = [
+        preset1,
+        preset2,
+        preset3
+    ]
+
+    target_preset = Preset()
+
+    # Parse any command-line arguments.
+    target_preset.parse_arguments()
+
+    #
+    target_preset.define_preset(preset0)
+    target_preset.execute()
+
+    # from pprint import pprint
+    for i in preset:
+        # pprint(i)
+        # print "------------------------------------------"
+        target_preset.define_preset(i)
+        target_preset.execute()
+
+        if i > 1:
+            print "Sleeping..."
+            sleep((topFadeTime / 10) + 1)
+            # sleep(2)
--- presets/all/Preset_WakeUp.py
+++ presets/all/Preset_WakeUp.py
@@ -1,32 +1,3 @@
 #!/usr/bin/python
 
-import PyHueAPI, os, sys
-
-if __name__ == '__main__':
-    preset = {
-        1: {'on': True, 'bri': 254, 'ct': 362},
-        3: {'on': True, 'bri': 253, 'ct': 303},
-        5: {'on': True, 'bri': 236, 'ct': 156},
-        4: {'on': True, 'bri': 236, 'ct': 156},
-        7: {'on': True, 'bri': 236, 'ct': 156},
-        6: {'on': True, 'bri': 236, 'ct': 156},
-        9: {'on': True, 'bri': 236, 'ct': 156},
-        8: {'on': True, 'bri': 236, 'ct': 156},
-
-        2: {'on': True, 'bri': 254, 'ct': 332},
-        10: {'on': False},
-        11: {'on': False},
-        12: {'on': False},
-        13: {'on': False},
-        14: {'on': False},
-    }
-
-    # TRAP
-    if (os.path.exists('/tmp/pyhueapi.disable')): sys.exit(0)
-
-    lights = PyHueAPI.Lights()
-    for i in range(1,15):
-        tmp = lights.get(i)
-        data = preset[i]
-
-        tmp.bulkSetState(data)
+print "Not used"
